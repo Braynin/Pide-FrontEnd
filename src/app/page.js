db1cd65f62c3ce8232af7d51270fad8f546b451d
Braynin <brayhannb@gmail.com>
2025-02-18 20:30:09 -0500

    mandanding los archivos creados

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
 import MainIndex from "./ui/components/mainIndex.js";
-import { getRestaurants } from "../app/api/restaurants.js";
-import { getProducts } from "../app/api/products.js";
 
 export default async function Home() {
-  const restaurants = await getRestaurants();
-  const products = await getProducts();
   return (
     <main>
       <div>
-        <MainIndex restaurants={restaurants} products={products} />
+        <MainIndex />
       </div>
     </main>
   );
